03542a65b0b5a314ef542e36ac6069d314f79ad8
coyotte508 <coyotte508@gmail.com>
2018-11-24 01:51:00 +0100

    Color update

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,10 @@ module.exports = {
   pwa: {
     // Should only be enabled for IOS >= 11.3
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black'
+    // appleMobileWebAppStatusBarStyle: 'black',
     // Settings should also be changed in public/manifest.json
     // name: 'The Pulsometer', // Also edit in public/index.html
-    // themeColor: '#4DBA87',
-    // msTileColor: '#000000',
+    themeColor: '#D4FE5B',
+    msTileColor: '#B40486',
   }
 }
